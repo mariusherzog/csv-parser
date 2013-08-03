fbaf8a7701e70a834a93515f1890d3cf9a1c9316
Marius Herzog <priv@mariusherzog.net>
2013-08-03 17:14:35 +0200

    added template specialization for std::string, making it possible to return raw rows.

--- csv.hpp
+++ csv.hpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <tuple>
 #include <stdexcept>
+#include <algorithm>
+
 
 template <typename T, typename... Ts>
 class csv
@@ -54,14 +56,15 @@ class csv
 
 
    private:
-      std::fstream csvf;
+      std::ifstream csvf;
       char sep;
       bool header;
       std::string headerns;
 
       template<std::size_t I = 0, typename... Tp>
       inline typename std::enable_if<I == sizeof...(Tp), void>::type
-      fill(std::tuple<Tp...>&, std::stringstream&) { }
+      fill(std::tuple<Tp...>&, std::stringstream&)
+      { }
 
       template<std::size_t I = 0, typename... Tp>
       inline typename std::enable_if<(I < sizeof...(Tp)), void>::type
@@ -79,5 +82,59 @@ class csv
       csv(const csv &rhs);
 
 };
+
+
+
+template <>
+class csv<std::string>
+{
+   public:
+      explicit csv(std::string file, char sep = ',', bool header = false):
+         csvf(file),
+         sep(sep),
+         header(header)
+      {
+         if (!csvf.is_open()) {
+            throw std::runtime_error("Could not open file!\n");
+         }
+
+         if (header) {
+            std::getline(csvf, headerns);
+         } else {
+            headerns = "";
+         }
+      }
+
+      std::string readline()
+      {
+         std::string row;
+         std::getline(csvf, row);
+         return row;
+      }
+
+      bool has_dataset()
+      {
+         std::string buf;
+         std::getline(csvf, buf);
+         csvf.seekg(-(buf.length()+1), std::ios_base::cur);
+         return !(buf.empty());
+      }
+
+      std::string headernames() const
+      {
+         return headerns;
+      }
+
+
+   private:
+      std::ifstream csvf;
+      char sep;
+      bool header;
+      std::string headerns;
+
+      csv& operator=(const csv &rhs);
+      csv(const csv &rhs);
+};
+
 
 #endif // CSV_HPP
--- main.cpp
+++ main.cpp
@@ -13,22 +13,25 @@ struct testy
 {
       int x;
       std::string y;
-      char z;
+      double z;
 
-      testy(int x, std::string y, char z):
+      testy(int x, std::string y, double z):
          x(x), y(y), z(z)
       {}
 };
 
 
+
+
 int main(int argc, char *argv[])
 {
    // data type for each field
-   csv<int, std::string, char> f("../csv/tcsv.csv");
+   {
+   csv<int, std::string, double> f("../csv/tcsv.csv");
 
    std::vector<testy> testies;
    while (f.has_dataset()) {
-      std::tuple<int, std::string, char> t = f.readline();
+      std::tuple<int, std::string, double> t = f.readline();
       testies.push_back(testy(std::get<0>(t),
                         std::get<1>(t),
                         std::get<2>(t)));
@@ -36,6 +39,12 @@ int main(int argc, char *argv[])
 
    for (testy &t : testies) {
       std::cout << t.x << " " << t.y << " " << t.z << "\n";
+   }
+   }
+
+   csv<std::string> g("../csv/tcsv.csv");
+   while (g.has_dataset()) {
+      std::cout << g.readline() << "\n";
    }
 
 }
